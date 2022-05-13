dfbd4120eedb4988d6a36711d56595b031882250
IMAMSGIT <imamhosain95@gmail.com>
2022-05-13 18:35:00 +0600

    example of change

--- variables.js
+++ variables.js
@@ -1,4 +1,9 @@
 let name1;
 console.log(name1);
 let name2 = 'Hmm';
-console.log(name2) 
+console.log(name2)
+// can't be a reserved name
+//should be meaningful
+//can't start with a number(1name)
+//Are case-sensitive
+let firstName, lastName
