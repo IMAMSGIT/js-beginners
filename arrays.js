cdd3eabff0f0e9a6233ebd6acc4eebaed6d2cbf1
IMAMSGIT <imamhosain95@gmail.com>
2022-05-15 20:26:35 +0600

    last

--- arrays.js
+++ arrays.js
@@ -1,2 +1,4 @@
+//Array is also an object
 let selectedColors = ['red', 'blue', 34];
+console.log(selectedColors.length)
 console.log(selectedColors[0])
---
+++ functions.js
@@ -0,0 +1,10 @@
+// function greet() {
+//     console.log('Hello World');
+// }
+// greet()
+
+function greeting(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+greeting('John')
+greeting('Cena')
