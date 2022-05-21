1603bab7430ab5be253701ccca00b83eb3b60e94
IMAMSGIT <imamhosain95@gmail.com>
2022-05-21 19:25:50 +0600

    uncomplete control flow

---
+++ Control Flow/for.js
@@ -0,0 +1,9 @@
+// JS has 5 loops : For,While,Do...while,For...in,For...of
+
+for (let i = 0; i < 5; i++) {
+    console.log('Hello World', i);
+}
+
+for (let j = 1; j <= 5; j++) {
+    if (j % 2 == 0) console.log(j);
+}
--- Control Flow/if-else.js
+++ Control Flow/if-else.js
@@ -1,2 +1,10 @@
 // js has 2 type of conditional statements
 // if else and swith case
+let hour = 10;
+
+if (hour > 10 && hour < 12)
+    console.log('Morning');
+else if (hour >= 12 && hour < 18)
+    console.log('Afternoon');
+else
+    console.log('Evening');
