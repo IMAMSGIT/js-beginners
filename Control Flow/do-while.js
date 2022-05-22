833e188913137c5547f71cbb54ad4b56812c472b
IMAMSGIT <imamhosain95@gmail.com>
2022-05-23 00:52:06 +0600

    just regular

---
+++ Control Flow/do-while.js
@@ -0,0 +1,13 @@
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
+
+
+// at least one time execution for do while loop
+let j = 7;
+do {
+    console.log(j);
+    j++;
+} while (j < 5);
---
+++ Control Flow/for-in.js
@@ -0,0 +1,12 @@
+let person = {
+    name: 'Adiat',
+    age: 33
+};
+
+for (let firstPart in person)
+    console.log(firstPart, person[firstPart]);
+
+
+const colors = ['red', 'green', 'blue']
+for (let index in colors)
+    console.log(index)
