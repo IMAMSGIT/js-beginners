0dd694b805263cb019562332a0e79afafd913f8e
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-06-12 02:47:56 +0600

    math and string objects

---
+++ Objects/BuiltinObjects/math.js
@@ -0,0 +1,3 @@
+console.log(Math.random());
+console.log(Math.round(1.4));
+console.log(Math.max(2, 3, 4, 6, 7));
---
+++ Objects/BuiltinObjects/string.js
@@ -0,0 +1,10 @@
+// string primitive
+const message = 'hello';
+
+
+//string object
+const anotherNew = new String('Hi');
+
+
+console.log(typeof (message));
+console.log(typeof (anotherNew));
