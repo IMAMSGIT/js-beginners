494710978057f85db9a04e63844b0b6f8530e235
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-06-15 00:55:04 +0600

    a little

---
+++ Objects/ex-object-equality.js
@@ -0,0 +1,8 @@
+function Address(street, city, address) {
+    this.street = street;
+    this.city = city;
+    this.address = address;
+}
+let address1 = new Address('a', 'b', 'c');
+let address2 = new Address('a', 'b', 'c');
+console.log(address1);
