7e2693e7f1669e102c8d5a4c04ee10722eff0e82
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-06-16 02:33:35 +0600

    partially at finish point

---
+++ Objects/blog-post-object.js
@@ -0,0 +1,15 @@
+let post = {
+    title: 'a',
+    body: 'b',
+    authos: 'r',
+    views: 12,
+    comments: [{
+        author: 'a',
+        body: 'f'
+    }, {
+        author: 'b',
+        body: 'g'
+    }],
+    isLive: true
+};
+console.log(post);
--- Objects/ex-object-equality.js
+++ Objects/ex-object-equality.js
@@ -3,6 +3,22 @@ function Address(street, city, address) {
     this.city = city;
     this.address = address;
 }
+
+
+function areEqual(address1, address2) {
+    return address1.street === address2.street &&
+        address1.city === address2.city &&
+        address1.address === address2.address;
+
+}
+
+function areSame(address1, address2) {
+    return address1 === address2;
+}
+
 let address1 = new Address('a', 'b', 'c');
 let address2 = new Address('a', 'b', 'c');
-console.log(address1);
+let address3 = address2;
+console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
+console.log(areSame(address2, address3));
