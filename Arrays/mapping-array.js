818fd865823e93a0e444a46789ec5c3810e11ff2
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-06-19 01:53:25 +0600

    exercise left

--- Arrays/mapping-array.js
+++ Arrays/mapping-array.js
@@ -1,1 +1,31 @@
-cons
+const numbers = [1, -1, 2, 3];
+const filtered = numbers.filter(n =>
+    n >= 0
+);
+
+
+const items = filtered.map(n => ({
+    value: n // to return have to rap by parenthesis
+}));
+
+
+console.log(items);
+console.log(items[0]);
+
+
+
+//chained
+const chainedItems = numbers
+    .filter(n => n >= 0)
+    .map(n => ({
+        value: n
+    }))
+    .filter(obj => obj.value > 1);
+
+
+console.log(chainedItems);
+
+// const items = filtered.map(n => '<li>' + n + '</li>');
+
+// const html = '<ul>' + items.join('') + '</ul>'; // to remove the comma
+// console.log(html);
---
+++ Arrays/reducing-array.js
@@ -0,0 +1,13 @@
+const numbers = [1, -1, 2, 3];
+let sum = 0;
+for (let n of numbers)
+    sum += n;
+
+
+console.log(sum);
+
+const sameSum = numbers.reduce((accumulator, currentValue) =>
+    accumulator + currentValue);
+
+
+console.log(sameSum);
