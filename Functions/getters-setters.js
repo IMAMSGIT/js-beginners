5cc77230ddd00c383796b2582b45528f0de97934
IMAMSGIT <99043428+IMAMSGIT@users.noreply.github.com>
2022-06-21 15:42:20 +0600

    getters and setters completed

--- Functions/getters-setters.js
+++ Functions/getters-setters.js
@@ -1,9 +1,17 @@
 const person = {
     firstName: 'Mosh',
     lastName: 'John',
-    fullName() {
+    // now it's a getter
+    get fullName() {
         return `${person.firstName} ${person.lastName}`
+    },
+    set fullName(value) {
+        const parts = value.split(' ');
+        this.firstName = parts[0];
+        this.lastName = parts[1];
     }
 };
 
-console.log(person.fullName);
+person.fullName = 'smith adam';
+
+console.log(person);
